5c0d53429eef69fddafc9054337cad5b024b2374
Jason <jui11@uclive.ac.nz>
2021-08-22 20:35:46 +1200

    robust lsr updated

--- src/robust_lsr.py
+++ src/robust_lsr.py
@@ -22,11 +22,12 @@
 #  Module Description:
 #
 #
+from re import X
 import matplotlib.pyplot as plt
 from scipy.optimize import least_squares
 import numpy as np
 
-# Load data
+# Load data sonar1
 filename = 'part_a/sonar1-calibration.csv'
 data = np.loadtxt(filename, delimiter=',', skiprows=1)
 
@@ -35,24 +36,51 @@ data = np.loadtxt(filename, delimiter=',', skiprows=1)
 #    sonar1, sonar2 = data.T
 distance, sonar1 = data.T
 
+# Load data ir3
+filename = 'part_a/ir3-calibration.csv'
+data = np.loadtxt(filename, delimiter=',', skiprows=1)
+distance, ir3 = data.T
 
-def fun(k1, k2, x):
-    return k1/(k2+x)
+def fun_sonar1(x, t, y):
+    return (x[0] * t) + x[1] - y
 
-
-x0 = np.ones(1)
-res_robust = least_squares(fun, x0, loss='soft_l1', f_scale=0.1, args=(distance, sonar1))
-
-y = generate_data(*res_robust.k1, *res_robust.k2, *res_robust.x)
-plt.plot(distance, y, color="red")
-plt.ylabel('da')
-plt.show()
-print(y)
-
-def generate_data(k1, k2, x, noise=0, n_outliers=0, random_state=0):
-    y = k1/(k2+x)
+def generate_data_sonar1(x, k1, k2, noise=0, n_outliers=0, random_state=0):
+    y = (k1 * x) + k2
     #rnd = np.random.RandomState(random_state)
     #error = noise * rnd.randn(t.size)
     #outliers = rnd.randint(0, t.size, n_outliers)
     #error[outliers] *= 35
     return y
+
+
+x0 = [1, 1, 1]
+
+res_robust_sonar1 = least_squares(fun_sonar1, x0, loss='soft_l1', f_scale=0.1, args=(distance, sonar1))
+
+if (res_robust_sonar1.success):
+    print("Success")
+    print("x = ", res_robust_sonar1.x) 
+    print("y = ", res_robust_sonar1.fun)
+
+y_n_sonar1 = generate_data_sonar1(distance, *res_robust_sonar1.x)
+#plt.plot(distance, sonar1, '.', markersize=1, color="black")
+#plt.plot(distance, y_n_sonar1, color="red")
+
+
+def fun_ir3(x, t, y):
+    return x[0]/(t + x[1]) - y
+
+def generate_data_ir3(x, k1, k2, noise=0, n_outliers=0, random_state=0):
+    y = k1/(x + k2)
+    #rnd = np.random.RandomState(random_state)
+    #error = noise * rnd.randn(t.size)
+    #outliers = rnd.randint(0, t.size, n_outliers)
+    #error[outliers] *= 35
+    return y
+
+res_robust_ir3 = least_squares(fun_ir3, x0, loss='soft_l1', f_scale=1, args=(distance, ir3))
+y_n_ir3 = generate_data_ir3(distance, *res_robust_ir3.x)
+plt.plot(distance, ir3, '.', markersize=1, color="black")
+plt.plot(distance, y_n_ir3, color="red")
+plt.ylabel('da')
+plt.show()
