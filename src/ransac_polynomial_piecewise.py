a43bada45d53f2f03085b2d531ce86645b67f1aa
Randipa G <randipa99@gmail.com>
2021-08-24 18:31:20 +1200

    Added error calculation, more plotting and histograms

--- src/ransac_polynomial_piecewise.py
+++ src/ransac_polynomial_piecewise.py
@@ -1,7 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider
-from sklearn.linear_model import RANSACRegressor
 from sklearn.linear_model import RANSACRegressor
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
@@ -20,22 +18,35 @@ def find_nearest_index(array, value):
     return idx
 
 brkpt = find_nearest_index(distance, 0.4)
-print(brkpt)
 
-model_lin_x = distance[0:brkpt]
-model_lin_y = raw_ir3[0:brkpt]
+lin_dist = distance[0:brkpt]
+lin_meas = raw_ir3[0:brkpt]
 
-model_pol_x = distance
-model_pol_y = raw_ir3
+poly_dist = distance
+poly_meas = raw_ir3
 
-ransac_lin = RANSACRegressor()
-ransac_lin.fit(model_lin_x.reshape(-1,1), model_lin_y)
-pred_lin = ransac_lin.predict(model_lin_x.reshape(-1,1))
+lin_ransac = RANSACRegressor(random_state=1)
+lin_ransac.fit(lin_dist.reshape(-1,1), lin_meas)
+lin_pred = lin_ransac.predict(lin_dist.reshape(-1,1))
+lin_inlier_mask = lin_ransac.inlier_mask_
+lin_outlier_mask = np.logical_not(lin_inlier_mask)
 
-ransac_pol = make_pipeline(PolynomialFeatures(3), RANSACRegressor())
-ransac_pol.fit(model_pol_x.reshape(-1,1), model_pol_y)
+poly_ransac = make_pipeline(PolynomialFeatures(2), RANSACRegressor(random_state=1))
+poly_ransac.fit(poly_dist.reshape(-1,1), poly_meas)
 #mse = mean_squared_error(ransac.predict(distance.reshape(-1,1)), raw_ir3)
-pred_pol = ransac_pol.predict(model_pol_x.reshape(-1,1))
+poly_pred = poly_ransac.predict(poly_dist.reshape(-1,1))
+pol_inlier_mask = poly_ransac.steps[1][1].inlier_mask_
+pol_outlier_mask = np.logical_not(pol_inlier_mask)
+
+poly_dist_inlier = np.delete(poly_dist, pol_outlier_mask)
+poly_meas_inlier = np.delete(poly_meas, pol_outlier_mask)
+poly_pred_inlier = poly_ransac.predict(poly_dist_inlier.reshape(-1,1))
+poly_error_inlier = poly_meas_inlier - poly_pred_inlier
+
+print("Distance", np.size(distance))
+print("Distance inlier size", np.size(poly_dist_inlier))
+print("Preditction inlier size", np.size(poly_pred_inlier))
+print("Error size", np.size(poly_error_inlier))
 
 fig, axes = plt.subplots(2, 3)
 fig.suptitle('Calibration data')
@@ -44,11 +55,14 @@ axes[0, 0].plot(distance, raw_ir1, '.', alpha=0.2)
 axes[0, 0].set_title('IR1')
 
 axes[0, 1].plot(distance, raw_ir2, '.', alpha=0.2)
+# axes[0, 1].plot(lin_dist, lin_pred)
+# axes[0, 1].plot(poly_dist, poly_pred)
+# axes[0, 1].scatter(pol_out_x_data, pol_out_y_data, color='red', marker='.')
 axes[0, 1].set_title('IR2')
 
 axes[0, 2].plot(distance, raw_ir3, '.', alpha=0.2)
-axes[0, 2].plot(model_lin_x, pred_lin)
-axes[0, 2].plot(model_pol_x, pred_pol)
+axes[0, 2].plot(lin_dist, lin_pred, color='orange')
+axes[0, 2].plot(poly_dist, poly_pred, color='red')
 axes[0, 2].set_title('IR3')
 
 axes[1, 0].plot(distance, raw_ir4, '.', alpha=0.2)
@@ -60,5 +74,18 @@ axes[1, 1].set_title('Sonar1')
 axes[1, 2].plot(distance, sonar2, '.', alpha=0.2)
 axes[1, 2].set_title('Sonar2')
 
+poly_dist1 = np.delete(poly_dist, pol_outlier_mask)
+poly_meas1 = np.delete(poly_meas, pol_outlier_mask)
+
+poly_dist2 = np.delete(poly_dist, pol_inlier_mask)
+poly_meas2 = np.delete(poly_meas, pol_inlier_mask)
+
+fig2, axes2 = plt.subplots(2)
+axes2[0].scatter(poly_dist_inlier, poly_error_inlier)
+axes2[1].hist(poly_error_inlier, 100)
+
+plt.figure()
+plt.scatter(poly_dist1, poly_meas1)
+plt.scatter(poly_dist2, poly_meas2)
 
 plt.show()
