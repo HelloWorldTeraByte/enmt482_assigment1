cf1afa9efc08cb91fcb76f5dc3d11626cd464552
Randipa G <randipa99@gmail.com>
2021-09-02 19:52:41 +1200

    Using variance of the estimator now

--- src_v2/ekf.py
+++ src_v2/ekf.py
@@ -35,17 +35,17 @@ index, time, distance, velocity_command, raw_ir1, raw_ir2, raw_ir3, raw_ir4, \
 step_num = 2000
 dt = 0.1
 
-motion_model_var = 2 * 10 ** -5
+motion_model_var = 5 * 10 ** -6
 
 sonar1_sen = Sonar1Sensor(distance, sonar1)
 ir3_sen = Ir3Sensor(distance, raw_ir3)
 ir4_sen = Ir4Sensor(distance, raw_ir4)
 
-mean_x_posterior = 0.1
-var_x_posterior = 10 ** 2
-
 mean_est = np.zeros(step_num)
 plot_k = np.zeros(step_num)
+
+mean_x_posterior = 0.1
+var_x_posterior = 10 ** 2
 
 for n in range(step_num):
     # Predict
@@ -57,23 +57,29 @@ for n in range(step_num):
     ir3_est = ir3_sen.x_est_mle(raw_ir3[n], mean_x_posterior)
     ir4_est = ir4_sen.x_est_mle(raw_ir4[n], mean_x_posterior)
 
-    w1 = 1 / sonar1_sen.error_var / (1 / sonar1_sen.error_var + 1 / ir3_sen.error_var + 1 / ir4_sen.error_var)
-    w2 = 1 / ir3_sen.error_var / (1 / sonar1_sen.error_var + 1 / ir3_sen.error_var + 1 / ir4_sen.error_var)
-    w3 = 1 / ir4_sen.error_var / (1 / sonar1_sen.error_var + 1 / ir3_sen.error_var + 1 / ir4_sen.error_var)
+    w1 = 1 / sonar1_sen.var_estimator() / (1 / sonar1_sen.var_estimator() + 1 / ir3_sen.var_estimator_at_x0(mean_x_posterior) + 1 / ir4_sen.var_estimator_at_x0(mean_x_posterior))
+    w2 = 1 / ir3_sen.var_estimator_at_x0(mean_x_posterior) / (1 / sonar1_sen.var_estimator() + 1 / ir3_sen.var_estimator_at_x0(mean_x_posterior) + 1 / ir4_sen.var_estimator_at_x0(mean_x_posterior))
+    w3 = 1 / ir4_sen.var_estimator_at_x0(mean_x_posterior) / (1 / sonar1_sen.var_estimator() + 1 / ir3_sen.var_estimator_at_x0(mean_x_posterior) + 1 / ir4_sen.var_estimator_at_x0(mean_x_posterior))
 
     mean_x_meas = w1 * sonar1_est + w2 * ir3_est + w3 * ir4_est
-    var_x_meas = 1 / ( 1 / sonar1_sen.var_estimator() + 1 / ir3_sen.var_estimator_at_x0(mean_x_posterior) + 1 / ir4_sen.var_estimator_at_x0(mean_x_posterior) )
+    var_x_meas = 1 / (1 / sonar1_sen.var_estimator() + 1 / ir3_sen.var_estimator_at_x0(mean_x_posterior) + 1 / ir4_sen.var_estimator_at_x0(mean_x_posterior) )
 
-    '''
     K = ( 1 / var_x_meas ) / (1 / var_x_meas + 1 / var_x_prior)
 
     mean_x_posterior = K * mean_x_meas + (1 - K ) * mean_x_prior
-    var_x_posterior =  1  / (1 / var_x_meas + 1 / var_x_prior)
-    '''
+    var_x_posterior = 1 / (1 / var_x_meas + 1 / var_x_prior)
 
+    if(mean_x_posterior < 0.1):
+        mean_x_posterior = 0.1
+    if(mean_x_posterior > 3):
+        mean_x_posterior = 3
+
+
+    '''
     K = var_x_prior / ( var_x_meas + var_x_prior )
     mean_x_posterior = mean_x_prior + K * ( mean_x_meas - mean_x_prior)
     var_x_posterior = (1 - K) * var_x_prior
+    '''
 
     mean_est[n] = mean_x_posterior
     plot_k[n] = K
